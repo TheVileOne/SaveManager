46b951f2ed0470a3af304e33c11e371566c44287
Fluffball <xeolite@yahoo.com>
2024-03-25 16:27:32 -0400

    Rename project

---
+++ SaveManager/FileInfoResult.cs
@@ -0,0 +1,12 @@
+﻿namespace SaveManager
+{
+    public class FileInfoResult
+    {
+        //Version info
+        public string CurrentVersion;
+        public string LastVersion;
+        //Path info
+        public string CurrentVersionPath;
+        public string LastVersionPath;
+    }
+}
---
+++ SaveManager/SaveUtils.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace SaveManager
+{
+    public static class SaveUtils
+    {
+        public static string[] SaveFiles =
+        {
+            "sav",
+            "sav2",
+            "sav3",
+            "expCore",
+            "expCore1",
+            "expCore2",
+            "expCore3",
+            "exp",
+            "exp1"
+        };
+
+        public static bool BackupSaves(string backupPath)
+        {
+            try
+            {
+                Directory.CreateDirectory(backupPath);
+
+                foreach (string file in SaveFiles) //Copy each save file into the specified path
+                    CopySaveFile(file, backupPath, false);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Plugin.Logger.LogError("Error occurred while copying save backups");
+                Plugin.Logger.LogError(ex);
+                return false;
+            }
+        }
+
+        public static bool RestoreFromBackup(string backupPath)
+        {
+            try
+            {
+                foreach (string file in SaveFiles) //Copy each save file into the specified path
+                    CopySaveFile(file, backupPath, false);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Plugin.Logger.LogError("Error occurred while copying save backups");
+                Plugin.Logger.LogError(ex);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Moves a save file between the persistent data path, and the specified backup path
+        /// </summary>
+        /// <param name="filename">The file to copy</param>
+        /// <param name="backupPath">The path where file backups are stored</param>
+        /// <param name="copyingFromBackups">Changes the move destination</param>
+        public static void CopySaveFile(string filename, string backupPath, bool copyingFromBackups)
+        {
+            string sourcePath, destPath;
+
+            if (copyingFromBackups)
+            {
+                sourcePath = Path.Combine(backupPath, filename);
+                destPath = Path.Combine(Application.persistentDataPath, filename);
+            }
+            else
+            {
+                sourcePath = Path.Combine(Application.persistentDataPath, filename);
+                destPath = Path.Combine(backupPath, filename);
+            }
+
+            if (File.Exists(sourcePath))
+                FileSystemUtils.SafeCopyFile(sourcePath, destPath);
+        }
+    }
+}
